d0859e1759cd8b1418da8dac3a1d6973dbae07ea
HTG <brain1401@gmail.com>
2025-04-30 16:31:36 +0900

    1일차 구현

--- src/main/java/survival/model/game/GameState.java
+++ src/main/java/survival/model/game/GameState.java
@@ -1,5 +1,7 @@
 package survival.model.game;
 
+import survival.util.Constants;
+
 /**
  * 게임 상태를 관리하는 클래스
  */
@@ -10,26 +12,40 @@ public class GameState {
     private boolean gameOver;
     private boolean victory;
     private GameEndState endState;
-    
+
     /**
      * 기본 생성자
      */
     public GameState() {
-        // 생성자 구현 부분
+
     }
-    
+
+    /**
+     * 
+     */
+    public GameState(int day, Player player, boolean gameOver, boolean victory, GameEndState endState) {
+        this.day = day;
+        this.player = player;
+        this.gameOver = gameOver;
+        this.victory = victory;
+        this.endState = endState;
+    }
+
     /**
      * 다음 날로 진행
      * - day 필드 1 증가
-     * - 날짜 변경에 따른 플레이어 상태 업데이트 (허기, 피로도 등)
+     * - 날짜 변경에 따른 플레이어 상태 업데이트 (피로도 등)
      * - 게임 상태 관련 로직 처리
      */
     public void nextDay() {
-        // 메소드 구현 부분
+        day++;
+        player.addAP(Constants.AP_PER_DAY);
+        // 기타 로직 추가 필요시 추가해야 함
     }
-    
+
     /**
      * 게임 종료 처리
+     * 
      * @param endState 게임 종료 상태
      */
     public void endGame(GameEndState endState) {
@@ -37,60 +53,67 @@ public class GameState {
         this.victory = (endState == GameEndState.VICTORY);
         this.endState = endState;
     }
-    
+
     /**
      * 게임 종료 처리 (기존 함수와의 호환성 유지)
+     * 
      * @param victory 승리 여부
      */
     public void endGame(boolean victory) {
         endGame(victory ? GameEndState.VICTORY : GameEndState.DEATH);
     }
-    
+
     /**
      * 게임 종료 여부 확인
+     * 
      * @return 게임 종료 여부
      */
     public boolean isGameOver() {
         return gameOver;
     }
-    
+
     /**
      * 승리 여부 확인
+     * 
      * @return 승리 여부
      */
     public boolean isVictory() {
         return victory;
     }
-    
+
     /**
      * 게임 종료 상태 반환
+     * 
      * @return 게임 종료 상태
      */
     public GameEndState getEndState() {
         return endState;
     }
-    
+
     /**
      * 현재 일차 반환
+     * 
      * @return 일차
      */
     public int getDay() {
         return day;
     }
-    
+
     /**
      * 플레이어 반환
+     * 
      * @return 플레이어
      */
     public Player getPlayer() {
         return player;
     }
-    
+
     /**
      * 승리 조건 확인
+     * 
      * @return 승리 조건 충족 여부
      */
     public boolean checkVictoryCondition() {
         return false; // 임시 반환값
     }
-} 
+}
