820724952e51221bf4a7a44bd1dfdd9d2363a2d8
HTG <brain1401@gmail.com>
2025-05-01 10:58:31 +0900

    CraftingController 클래스 수정: 뗏목 아이템 추가 및 제작 로직 구현, 자원 요구사항 확인 메소드 추가. 불필요한 메소드 제거 및 관련 주석 업데이트.

--- src/main/java/survival/controller/game/CraftingController.java
+++ src/main/java/survival/controller/game/CraftingController.java
@@ -3,7 +3,9 @@ package survival.controller.game;
 import survival.model.game.Player;
 import survival.model.game.Inventory;
 import survival.model.game.Item;
+import survival.model.game.ItemType;
 import survival.model.game.Recipe;
+import survival.model.game.ResourceType;
 import survival.view.GameView;
 
 import java.util.List;
@@ -16,69 +18,58 @@ import java.util.ArrayList;
  */
 public class CraftingController {
     // 필드
-    private Map<String, Item> availableItems;
     private GameView view;
-    
+    // 땟목 아이템
+    private Item raft;
+
     /**
      * 생성자
+     * 
      * @param view 게임 화면
      */
     public CraftingController(GameView view) {
         this.view = view;
-        this.availableItems = new HashMap<>();
-        initializeItems();
+
+        Map<ResourceType, Integer> resources = new HashMap<>();
+        resources.put(ResourceType.WOOD, 5);
+        resources.put(ResourceType.CLOTH, 3);
+        resources.put(ResourceType.STONE, 2);
+
+        raft = new Item("뗏목", ItemType.RAFT, new Recipe(resources));
     }
-    
-    /**
-     * 사용 가능한 모든 아이템 목록 반환
-     * @return 아이템 이름 목록
-     */
-    public List<String> getAvailableItems() {
-        return new ArrayList<>(availableItems.keySet());
-    }
-    
-    /**
-     * 현재 인벤토리로 제작 가능한 아이템 목록 반환
-     * @param inventory 인벤토리
-     * @return 제작 가능한 아이템 이름 목록
-     */
-    public List<String> getCraftableItems(Inventory inventory) {
-        return new ArrayList<>(); // 임시 반환값
-    }
-    
+
     /**
      * 아이템 제작 처리
-     * @param player 플레이어
+     * 
+     * @param player   플레이어
      * @param itemName 아이템 이름
      * @return 제작 성공 여부
      */
-    public boolean craftItem(Player player, String itemName) {
-        return false; // 임시 반환값
+    public void craftItem(Player player) {
+        Inventory inventory = player.getInventory();
+        
+        inventory.addItem(raft);
     }
-    
+
     /**
      * 제작 가능 여부 확인
-     * @param player 플레이어
+     * 
+     * @param player   플레이어
      * @param itemName 아이템 이름
      * @return 제작 가능 여부
      */
-    public boolean canCraft(Player player, String itemName) {
-        return false; // 임시 반환값
+    public boolean canCraft(Player player) {
+        Inventory inventory = player.getInventory();
+
+        Recipe raftRecipe = raft.getRecipe();
+        Map<ResourceType, Integer> requiredResources = raftRecipe.getResource();
+        
+        // 모든 자원이 충분한지 확인
+        // 하나라도 부족한 자원이 있으면 false 반환
+        return !requiredResources.entrySet().stream()
+                .anyMatch(requirement -> 
+                    inventory.getResourceCount(requirement.getKey().toString()) < requirement.getValue()
+                );
     }
-    
-    /**
-     * 아이템 레시피 반환
-     * @param itemName 아이템 이름
-     * @return 레시피
-     */
-    public Recipe getItemRecipe(String itemName) {
-        return null; // 임시 반환값
-    }
-    
-    /**
-     * 아이템 목록 초기화
-     */
-    public void initializeItems() {
-        // 메소드 구현 부분
-    }
-} 
+
+}
